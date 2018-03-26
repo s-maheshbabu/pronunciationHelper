54cbaa5cc13c3038777a3d316f7ab8ddc1ba0263
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2018-03-25 23:59:27 -0700

    Grunt based deployment configuration to directly deploy to lambda.

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+const grunt = require("grunt");
+grunt.loadNpmTasks("grunt-aws-lambda");
+
+grunt.initConfig({
+  lambda_invoke: {
+    default: {
+      options: {
+        file_name: "src/pronunciationHelper.js",
+        event: "test-data/event.json"
+      }
+    }
+  },
+  lambda_deploy: {
+    default: {
+      options: {
+        aliases: "beta",
+        enableVersioning: true
+      },
+      arn:
+        "arn:aws:lambda:us-east-1:837603326872:function:pronunciationHelper-development"
+    },
+    prod: {
+      options: {
+        aliases: "prod",
+        enableVersioning: true
+      },
+      arn: "arn:aws:lambda:us-east-1:837603326872:function:pronunciationHelper"
+    }
+  },
+  lambda_package: {
+    default: {},
+    prod: {}
+  }
+});
+
+grunt.registerTask("deploy", [
+  "lambda_package:default",
+  "lambda_deploy:default"
+]);
+grunt.registerTask("deploy_prod", [
+  "lambda_package:prod",
+  "lambda_deploy:prod"
+]);
