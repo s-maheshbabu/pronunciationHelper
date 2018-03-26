ab0aacf05175cd5f23e4b9be3175b49ca248dcf3
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2018-03-25 20:18:00 -0700

    Major refactor to use alexa-sdk functions.
    This makes the code considerably smaller by leveraging many built-in functions from alexa-sdk.
    
    Adjusted the tests accordingly.

--- src/pronunciationHelper.js
+++ src/pronunciationHelper.js
@@ -1,192 +1,63 @@
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = function(event, context) {
-  try {
-    console.log(
-      "event.session.application.applicationId=" +
-        event.session.application.applicationId
-    );
+const Alexa = require("alexa-sdk");
+const SKILL_ID = "amzn1.echo-sdk-ams.app.22799827-aae1-4115-9b48-e2b74e33ee03";
 
-    if (
-      event.session.application.applicationId !==
-      "amzn1.echo-sdk-ams.app.22799827-aae1-4115-9b48-e2b74e33ee03"
-    ) {
-      context.fail("Invalid Application ID");
-    }
-
-    if (event.session.new) {
-      onSessionStarted({ requestId: event.request.requestId }, event.session);
-    }
-
-    if (event.request.type === "LaunchRequest") {
-      onLaunch(event.request, event.session, function callback(
-        sessionAttributes,
-        speechletResponse
-      ) {
-        context.succeed(buildResponse(sessionAttributes, speechletResponse));
-      });
-    } else if (event.request.type === "IntentRequest") {
-      onIntent(event.request, event.session, function callback(
-        sessionAttributes,
-        speechletResponse
-      ) {
-        context.succeed(buildResponse(sessionAttributes, speechletResponse));
-      });
-    } else if (event.request.type === "SessionEndedRequest") {
-      onSessionEnded(event.request, event.session);
-      context.succeed();
-    }
-  } catch (e) {
-    context.fail("Exception: " + e);
+const handlers = {
+  "AMAZON.CancelIntent": function() {
+    this.emit(":tell", "");
+  },
+  HowToPronounceIntent: pronounceTheWord,
+  "AMAZON.HelpIntent": getHelpResponse,
+  "AMAZON.StopIntent": function() {
+    this.emit(":tell", "");
+  },
+  LaunchRequest: getWelcomeResponse,
+  SessionEndedRequest: function() {
+    console.log("Session ended! State when session ended: " + this.attributes);
+  },
+  Unhandled: function() {
+    this.emit("AMAZON.HelpIntent");
   }
 };
 
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-  console.log(
-    "onSessionStarted requestId=" +
-      sessionStartedRequest.requestId +
-      ", sessionId=" +
-      session.sessionId
-  );
-}
-
-/**
- * Called when the user launches the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-  console.log(
-    "onLaunch requestId=" +
-      launchRequest.requestId +
-      ", sessionId=" +
-      session.sessionId
-  );
-
-  // Dispatch to your skill's launch.
-  getWelcomeResponse(callback);
-}
-
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-  console.log(
-    "onIntent requestId=" +
-      intentRequest.requestId +
-      ", sessionId=" +
-      session.sessionId
-  );
-
-  var intent = intentRequest.intent,
-    intentName = intentRequest.intent.name;
-
-  // Dispatch to your skill's intent handlers
-  if ("HowToPronounceIntent" === intentName) {
-    pronounceTheWord(intent, session, callback);
-  } else if ("AMAZON.HelpIntent" === intentName) {
-    getHelpResponse(callback);
-  } else if ("AMAZON.StopIntent" === intentName) {
-    exitSkill(callback);
-  } else if ("AMAZON.CancelIntent" === intentName) {
-    exitSkill(callback);
-  } else {
-    throw "Invalid intent";
-  }
-}
-
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-  console.log(
-    "onSessionEnded requestId=" +
-      sessionEndedRequest.requestId +
-      ", sessionId=" +
-      session.sessionId
-  );
-  // Add cleanup logic here
-}
+exports.handler = function(event, context, callback) {
+  const alexa = Alexa.handler(event, context, callback);
+  alexa.appId = SKILL_ID;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
 
 // --------------- Functions that control the skill's behavior -----------------------
-
-function getWelcomeResponse(callback) {
-  // If we wanted to initialize the session to have some attributes we could add those here.
-  var sessionAttributes = {};
-  var cardTitle = "Welcome to Pronunciations";
-  var cardOutput =
-    "Examples: \n" +
-    "Pronounce D. O. G.\n" +
-    "How to pronounce B. I. T. S.\n" +
-    "What is the pronunciation for C. A. T.\n" +
-    "Ask pnonunciations to pronounce P. I. L. A. N. I.\n";
-  var speechOutput =
-    "<speak> Welcome to Pronunciations. " +
-    "You can say things like, pronounce B. I. T. S. </speak>";
-  // If the user either does not reply to the welcome message or says something that is not
-  // understood, they will be prompted again with this text.
-  var repromptText =
-    "<speak> What word do you want the pronunciation for? You can say things like, what is the pronunciation for P. I. L. A. N. I. </speak>";
-  var shouldEndSession = false;
-
-  callback(
-    sessionAttributes,
-    buildSpeechletResponse(
-      cardTitle,
-      cardOutput,
-      speechOutput,
-      repromptText,
-      shouldEndSession
-    )
+function getWelcomeResponse() {
+  this.emit(
+    ":askWithCard",
+    "Welcome to Pronunciations. You can say things like, pronounce B. I. T. S.",
+    "What word do you want the pronunciation for? You can say things like, what is the pronunciation for P. I. L. A. N. I.",
+    "Welcome to Pronunciations",
+    `Examples: 
+    Pronounce D. O. G.
+    How to pronounce B. I. T. S.
+    What is the pronunciation for C. A. T.
+    Ask pnonunciations to pronounce P. I. L. A. N. I.`
   );
 }
 
-function getHelpResponse(callback) {
-  var speechOutput =
-    "<speak> I can help you pronounce English words in American accent. You just need to spell the word you need the pronunciation for. " +
-    'For example, you can say, pronounce B. I. T. S. <break time="100ms"/> and I will tell you that it is pronounced as bits. So what word do you want me to pronounce? </speak>';
-  var repromptText =
-    "<speak> What word do you want the pronunciation for? You can say things like, what is the pronunciation for P. I. L. A. N. I. </speak>";
-  var shouldEndSession = false;
-
-  callback(
-    {},
-    buildSpeechletResponseWithoutCards(
-      speechOutput,
-      repromptText,
-      shouldEndSession
-    )
-  );
-}
-
-function exitSkill(callback) {
-  var speechOutput = "";
-  var repromptText = "";
-  var shouldEndSession = true;
-
-  callback(
-    {},
-    buildSpeechletResponseWithoutCards(
-      speechOutput,
-      repromptText,
-      shouldEndSession
-    )
+function getHelpResponse() {
+  this.emit(
+    ":ask",
+    `I can help you pronounce English words in American accent. You just need to spell the word you need the pronunciation for. For example, you can say, pronounce B. I. T. S. <break time="100ms"/> and I will tell you that it is pronounced as bits. So what word do you want me to pronounce?`,
+    `What word do you want the pronunciation for? You can say things like, what is the pronunciation for P. I. L. A. N. I.`
   );
 }
 
 /**
  * Pronounces the word or informs the user that no word exists with the spelling.
  */
-function pronounceTheWord(intent, session, callback) {
+function pronounceTheWord() {
+  const session = this.event.session;
+  const intent = this.event.request.intent;
+
   var cardTitle = "Pronunciations";
   var wordToBePronoucnedSlot = intent.slots.Spelling;
-  var repromptText = "";
-  var sessionAttributes = session.attributes;
-  var shouldEndSession = true;
-  var speechOutput = "";
-  var cardOutput = "";
 
   if (
     wordToBePronoucnedSlot !== undefined &&
@@ -194,145 +65,79 @@ function pronounceTheWord(intent, session, callback) {
   ) {
     var wordToBePronoucned = wordToBePronoucnedSlot.value;
     if (hasLowerCase(wordToBePronoucned)) {
-      sessionAttributes = incrementFailedAttemptsCount(sessionAttributes);
-
-      if (
-        !sessionAttributes ||
-        !sessionAttributes.numberOfFailedAttempts ||
-        sessionAttributes.numberOfFailedAttempts < 3
-      ) {
-        shouldEndSession = false;
-        speechOutput = "<speak> I didn't get that. Please try again. </speak>";
-        cardOutput =
-          'I heard "' +
-          wordToBePronoucned +
-          '" but I do not know how to pronounce it. Please try again.';
+      incrementFailedAttemptsCount(this.attributes);
+      if (isAttemptsRemaining(this.attributes)) {
+        this.emit(
+          ":askWithCard",
+          "I didn't get that. Please try again.",
+          "I didn't get the word you were asking for. Please try again.",
+          cardTitle,
+          `I heard "${wordToBePronoucned}" but I do not know how to pronounce it. Please try again.`
+        );
       } else {
-        speechOutput =
-          "<speak> Sorry, am having trouble understanding. Please try again later. Good bye. </speak>";
-        cardOutput =
-          'I heard "' +
-          wordToBePronoucned +
-          '" but I do not know how to pronounce it. Sorry.';
+        this.emit(
+          ":tellWithCard",
+          `Sorry, am having trouble understanding. Please try again later. Good bye.`,
+          cardTitle,
+          `I heard "${wordToBePronoucned}" but I do not know how to pronounce it. Sorry.`
+        );
       }
     } else if (hasWhiteSpaces(wordToBePronoucned)) {
       wordToBePronoucned = wordToBePronoucned.replace(/ /g, "");
-      speechOutput =
-        "<speak> I would pronounce it as " + wordToBePronoucned + ". </speak>";
-      cardOutput =
-        "Now that you know how to pronounce " +
-        wordToBePronoucned +
-        ', you can ask Alexa for its meaning by saying "Alexa, define ' +
-        wordToBePronoucned +
-        '"';
-      cardTitle = "Pronunciation of " + wordToBePronoucned;
+
+      this.emit(
+        ":tellWithCard",
+        `I would pronounce it as ${wordToBePronoucned}.`,
+        `Pronunciation of ${wordToBePronoucned}`,
+        `Now that you know how to pronounce ${wordToBePronoucned}, you can ask Alexa for its meaning by saying "Alexa, define ${wordToBePronoucned}"`
+      );
     } else {
-      speechOutput =
-        "<speak> It is pronounced as " + wordToBePronoucned + ". </speak>";
-      cardOutput =
-        "Now that you know how to pronounce " +
-        wordToBePronoucned +
-        ', you can ask Alexa for its meaning by saying "Alexa, define ' +
-        wordToBePronoucned +
-        '"';
-      cardTitle = "Pronunciation of " + wordToBePronoucned;
+      this.emit(
+        ":tellWithCard",
+        `It is pronounced as ${wordToBePronoucned}.`,
+        `Pronunciation of ${wordToBePronoucned}`,
+        `Now that you know how to pronounce ${wordToBePronoucned}, you can ask Alexa for its meaning by saying "Alexa, define ${wordToBePronoucned}"`
+      );
     }
   } else {
-    sessionAttributes = incrementFailedAttemptsCount(sessionAttributes);
+    incrementFailedAttemptsCount(this.attributes);
 
-    if (
-      !sessionAttributes ||
-      !sessionAttributes.numberOfFailedAttempts ||
-      sessionAttributes.numberOfFailedAttempts < 3
-    ) {
-      shouldEndSession = false;
-      speechOutput = "<speak> I didn't get that. Please try again. </speak>";
-      cardOutput =
-        "Am sorry, am having trouble understanding. Please try again.";
+    if (isAttemptsRemaining(this.attributes)) {
+      this.emit(
+        ":askWithCard",
+        "I didn't get that. Please try again.",
+        "I didn't get the word you were asking for. Please try again.",
+        cardTitle,
+        "Am sorry, am having trouble understanding. Please try again."
+      );
     } else {
-      speechOutput =
-        "<speak> Sorry, am having trouble understanding. Please try again later. Good bye </speak>";
-      cardOutput =
-        "Am sorry, am having trouble understanding. Please try again.";
+      this.emit(
+        ":tellWithCard",
+        "Sorry, am having trouble understanding. Please try again later. Good bye.",
+        cardTitle,
+        "Am sorry, am having trouble understanding. Please try again."
+      );
     }
   }
-
-  callback(
-    sessionAttributes,
-    buildSpeechletResponse(
-      cardTitle,
-      cardOutput,
-      speechOutput,
-      repromptText,
-      shouldEndSession
-    )
-  );
 }
 
-function incrementFailedAttemptsCount(sessionAttributes) {
-  if (sessionAttributes && sessionAttributes.numberOfFailedAttempts) {
-    sessionAttributes.numberOfFailedAttempts++;
-    return sessionAttributes;
+function isAttemptsRemaining(attributes) {
+  if (
+    typeof attributes["numberOfFailedAttempts"] === "number" &&
+    attributes["numberOfFailedAttempts"] >= 3
+  ) {
+    return false;
   }
-  return {
-    numberOfFailedAttempts: 1
-  };
+  return true;
 }
 
-// --------------- Helpers that build all of the responses -----------------------
-function buildSpeechletResponse(
-  cardTitle,
-  cardOutput,
-  speechOutput,
-  repromptText,
-  shouldEndSession
-) {
-  return {
-    outputSpeech: {
-      type: "SSML",
-      ssml: speechOutput
-    },
-    card: {
-      type: "Simple",
-      title: cardTitle,
-      content: cardOutput
-    },
-    reprompt: {
-      outputSpeech: {
-        type: "SSML",
-        ssml: repromptText
-      }
-    },
-    shouldEndSession: shouldEndSession
-  };
-}
-
-function buildSpeechletResponseWithoutCards(
-  speechOutput,
-  repromptText,
-  shouldEndSession
-) {
-  return {
-    outputSpeech: {
-      type: "SSML",
-      ssml: speechOutput
-    },
-    reprompt: {
-      outputSpeech: {
-        type: "SSML",
-        ssml: repromptText
-      }
-    },
-    shouldEndSession: shouldEndSession
-  };
-}
-
-function buildResponse(sessionAttributes, speechletResponse) {
-  return {
-    version: "1.0",
-    sessionAttributes: sessionAttributes,
-    response: speechletResponse
-  };
+function incrementFailedAttemptsCount(attributes) {
+  // TODO Is this right?
+  if (typeof attributes["numberOfFailedAttempts"] === "number") {
+    attributes["numberOfFailedAttempts"] += 1;
+  } else {
+    attributes["numberOfFailedAttempts"] = 1;
+  }
 }
 
 // --------------- Utility functions -----------------------
--- test/pronunciationHelper.test.js
+++ test/pronunciationHelper.test.js
@@ -20,25 +20,6 @@ afterEach(function() {
   decache("../test-data/event");
 });
 
-it("should fail when we receive an incorrect applicationId", function() {
-  const event = require("../test-data/event");
-  event.session.application.applicationId = "invalid application id";
-
-  const failSpy = sinon.spy(context, "fail");
-
-  unitUnderTest.handler(event, context);
-  expect(failSpy.calledWith("Invalid Application ID")).to.be.true;
-});
-
-it("should fail when we receive an unknown intent", function() {
-  const event = require("../test-data/unknown_intent_event");
-
-  const failSpy = sinon.spy(context, "fail");
-
-  unitUnderTest.handler(event, context);
-  assert(failSpy.calledOnce);
-});
-
 it("should increment the failure attempts count and reprompt the user if the spelling slot is missing altogether", function() {
   const event = require("../test-data/event");
   event.request.intent.slots.Spelling = undefined;
@@ -50,6 +31,7 @@ it("should increment the failure attempts count and reprompt the user if the spe
 
   const argument = succeedSpy.args[0][0];
   const sessionAttributesUsed = argument.sessionAttributes;
+  console.log(sessionAttributesUsed.numberOfFailedAttempts);
   expect(sessionAttributesUsed.numberOfFailedAttempts).to.equal(1);
 
   const responseUsed = argument.response;
@@ -62,7 +44,9 @@ it("should increment the failure attempts count and reprompt the user if the spe
   expect(outputSpeech.type).to.equal("SSML");
 
   const reprompt = responseUsed.reprompt;
-  expect(reprompt.outputSpeech.ssml).to.equal("");
+  expect(reprompt.outputSpeech.ssml).to.equal(
+    "<speak> I didn't get the word you were asking for. Please try again. </speak>"
+  );
   expect(reprompt.outputSpeech.type).to.equal("SSML");
 
   const card = responseUsed.card;
@@ -96,7 +80,9 @@ it("should increment the failure attempts count and reprompt the user if the spe
   expect(outputSpeech.type).to.equal("SSML");
 
   const reprompt = responseUsed.reprompt;
-  expect(reprompt.outputSpeech.ssml).to.equal("");
+  expect(reprompt.outputSpeech.ssml).to.equal(
+    "<speak> I didn't get the word you were asking for. Please try again. </speak>"
+  );
   expect(reprompt.outputSpeech.type).to.equal("SSML");
 
   const card = responseUsed.card;
@@ -119,6 +105,7 @@ it("should create a failure attempts count in session attributes if the input ha
   const succeedSpy = sinon.spy(context, "succeed");
   for (let i = 0; i < wordsWithLowerCaseCharacters.length; i++) {
     event.request.intent.slots.Spelling.value = wordsWithLowerCaseCharacters[i];
+    event.session.attributes = undefined;
 
     succeedSpy.reset();
     unitUnderTest.handler(event, context);
@@ -138,7 +125,9 @@ it("should create a failure attempts count in session attributes if the input ha
     expect(outputSpeech.type).to.equal("SSML");
 
     const reprompt = responseUsed.reprompt;
-    expect(reprompt.outputSpeech.ssml).to.equal("");
+    expect(reprompt.outputSpeech.ssml).to.equal(
+      "<speak> I didn't get the word you were asking for. Please try again. </speak>"
+    );
     expect(reprompt.outputSpeech.type).to.equal("SSML");
 
     const card = responseUsed.card;
@@ -186,9 +175,7 @@ it("should increment the failure attempts count in session attributes each time 
   );
   expect(outputSpeech.type).to.equal("SSML");
 
-  const reprompt = responseUsed.reprompt;
-  expect(reprompt.outputSpeech.ssml).to.equal("");
-  expect(reprompt.outputSpeech.type).to.equal("SSML");
+  expect(responseUsed.reprompt).to.be.undefined;
 
   const card = responseUsed.card;
   expect(card.title).to.equal(`Pronunciations`);
@@ -230,13 +217,11 @@ it("should increment the failure attempts count in session attributes each time 
 
   const outputSpeech = responseUsed.outputSpeech;
   expect(outputSpeech.ssml).to.equal(
-    "<speak> Sorry, am having trouble understanding. Please try again later. Good bye </speak>"
+    "<speak> Sorry, am having trouble understanding. Please try again later. Good bye. </speak>"
   );
   expect(outputSpeech.type).to.equal("SSML");
 
-  const reprompt = responseUsed.reprompt;
-  expect(reprompt.outputSpeech.ssml).to.equal("");
-  expect(reprompt.outputSpeech.type).to.equal("SSML");
+  expect(responseUsed.reprompt).to.be.undefined;
 
   const card = responseUsed.card;
   expect(card.title).to.equal(`Pronunciations`);
@@ -248,6 +233,37 @@ it("should increment the failure attempts count in session attributes each time 
 
 it("handles the AMAZON.HelpIntent properly", function() {
   const event = require("../test-data/help_intent_event");
+  const succeedSpy = sinon.spy(context, "succeed");
+
+  unitUnderTest.handler(event, context);
+
+  assert(succeedSpy.calledOnce);
+
+  const argument = succeedSpy.args[0][0];
+  const sessionAttributesUsed = argument.sessionAttributes;
+  assert(
+    Object.keys(sessionAttributesUsed).length === 0 &&
+      sessionAttributesUsed.constructor === Object
+  );
+
+  const responseUsed = argument.response;
+  assert(!responseUsed.shouldEndSession);
+
+  const outputSpeech = responseUsed.outputSpeech;
+  expect(outputSpeech.ssml).to.equal(
+    `<speak> I can help you pronounce English words in American accent. You just need to spell the word you need the pronunciation for. For example, you can say, pronounce B. I. T. S. <break time="100ms"/> and I will tell you that it is pronounced as bits. So what word do you want me to pronounce? </speak>`
+  );
+  expect(outputSpeech.type).to.equal("SSML");
+
+  const reprompt = responseUsed.reprompt;
+  expect(reprompt.outputSpeech.ssml).to.equal(
+    "<speak> What word do you want the pronunciation for? You can say things like, what is the pronunciation for P. I. L. A. N. I. </speak>"
+  );
+  expect(reprompt.outputSpeech.type).to.equal("SSML");
+});
+
+it("should treat unknown intents like Amazon.HelpIntent", function() {
+  const event = require("../test-data/unknown_intent_event");
   const succeedSpy = sinon.spy(context, "succeed");
 
   unitUnderTest.handler(event, context);
@@ -297,12 +313,10 @@ it("handles the AMAZON.CancelIntent properly", function() {
   assert(responseUsed.shouldEndSession);
 
   const outputSpeech = responseUsed.outputSpeech;
-  expect(outputSpeech.ssml).to.equal("");
+  expect(outputSpeech.ssml).to.equal("<speak>  </speak>");
   expect(outputSpeech.type).to.equal("SSML");
 
-  const reprompt = responseUsed.reprompt;
-  expect(reprompt.outputSpeech.ssml).to.equal("");
-  expect(reprompt.outputSpeech.type).to.equal("SSML");
+  expect(responseUsed.reprompt).to.be.undefined;
 });
 
 it("handles the AMAZON.StopIntent properly", function() {
@@ -325,12 +339,10 @@ it("handles the AMAZON.StopIntent properly", function() {
   assert(responseUsed.shouldEndSession);
 
   const outputSpeech = responseUsed.outputSpeech;
-  expect(outputSpeech.ssml).to.equal("");
+  expect(outputSpeech.ssml).to.equal("<speak>  </speak>");
   expect(outputSpeech.type).to.equal("SSML");
 
-  const reprompt = responseUsed.reprompt;
-  expect(reprompt.outputSpeech.ssml).to.equal("");
-  expect(reprompt.outputSpeech.type).to.equal("SSML");
+  expect(responseUsed.reprompt).to.be.undefined;
 });
 
 it("should render the welcome message on launch requests", function() {
@@ -367,7 +379,11 @@ it("should render the welcome message on launch requests", function() {
   expect(card.title).to.equal("Welcome to Pronunciations");
   expect(card.type).to.equal("Simple");
   expect(card.content).to.equal(
-    "Examples: \nPronounce D. O. G.\nHow to pronounce B. I. T. S.\nWhat is the pronunciation for C. A. T.\nAsk pnonunciations to pronounce P. I. L. A. N. I.\n"
+    `Examples: 
+    Pronounce D. O. G.
+    How to pronounce B. I. T. S.
+    What is the pronunciation for C. A. T.
+    Ask pnonunciations to pronounce P. I. L. A. N. I.`
   );
 });
 
@@ -386,7 +402,10 @@ it("should spell the words in the happy case", function() {
 
     const argument = succeedSpy.args[0][0];
     const sessionAttributesUsed = argument.sessionAttributes;
-    expect(sessionAttributesUsed).to.be.undefined;
+    assert(
+      Object.keys(sessionAttributesUsed).length === 0 &&
+        sessionAttributesUsed.constructor === Object
+    );
 
     const responseUsed = argument.response;
     assert(responseUsed.shouldEndSession);
@@ -397,9 +416,7 @@ it("should spell the words in the happy case", function() {
     );
     expect(outputSpeech.type).to.equal("SSML");
 
-    const reprompt = responseUsed.reprompt;
-    expect(reprompt.outputSpeech.ssml).to.equal("");
-    expect(reprompt.outputSpeech.type).to.equal("SSML");
+    expect(responseUsed.reprompt).to.be.undefined;
 
     const card = responseUsed.card;
     expect(card.title).to.equal(`Pronunciation of ${wordToBePronoucned}`);
@@ -435,7 +452,10 @@ separately which they do when they are asking for a pronunciation`, function() {
 
     const argument = succeedSpy.args[0][0];
     const sessionAttributesUsed = argument.sessionAttributes;
-    expect(sessionAttributesUsed).to.be.undefined;
+    assert(
+      Object.keys(sessionAttributesUsed).length === 0 &&
+        sessionAttributesUsed.constructor === Object
+    );
 
     const responseUsed = argument.response;
     assert(responseUsed.shouldEndSession);
@@ -446,9 +466,7 @@ separately which they do when they are asking for a pronunciation`, function() {
     );
     expect(outputSpeech.type).to.equal("SSML");
 
-    const reprompt = responseUsed.reprompt;
-    expect(reprompt.outputSpeech.ssml).to.equal("");
-    expect(reprompt.outputSpeech.type).to.equal("SSML");
+    expect(responseUsed.reprompt).to.be.undefined;
 
     const card = responseUsed.card;
     expect(card.title).to.equal(`Pronunciation of ${wordToBePronoucned}`);
