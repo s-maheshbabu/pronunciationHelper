73926bdc8f6c78c20b8dfb4bcff47e5a7f22fcdf
Mahesh Sabbavarapu <s.maheshbabu@hotmail.com>
2018-06-22 11:34:03 -0700

    second commit

--- test/spellcheck/SpellChecker.test.js
+++ test/spellcheck/SpellChecker.test.js
@@ -7,9 +7,21 @@ const dictionaryAdditions = require("spellcheck/DictionaryAdditions");
 
 var request = require("request");
 var cheerio = require("cheerio");
+var moment = require("moment");
 
 it("testing", async done => {
-  var searchTerm = "daca";
+  var start_day = moment("2010-01-01");
+  var end_day = moment("2010-03-01");
+
+  while (start_day.isBefore(end_day)) {
+    start_day = start_day.add(1, "d");
+
+    var url =
+      "https://www.merriam-webster.com/word-of-the-day/" +
+      start_day.format("YYYY-MM-DD");
+    console.log(url);
+  }
+
   var url = "https://www.merriam-webster.com/word-of-the-day/2018-06-14";
 
   request(url, function(err, resp, body) {
